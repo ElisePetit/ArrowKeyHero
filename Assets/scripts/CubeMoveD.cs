4091842aa2870d6ffd04cdc7d62b3508ad69710e
Elise Petit <epetit@amaris.com>
2018-06-08 14:28:54 +0200

    First commit

---
+++ Assets/scripts/CubeMoveD.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMoveD : MonoBehaviour {
+
+    public float speed = 8f;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        Renderer rend = GetComponent<Renderer>();
+
+        transform.Translate(new Vector3(0,0,-3f) * (speed * UnityEngine.Random.value + 2f) * Time.fixedDeltaTime);
+        CheckKeys(rend);
+
+        if (transform.position.z < -3)
+        {
+            Debug.Log("Boom ! Raté à droite");
+            GererCubes.score -= 1.5f;
+            Destroy(gameObject);
+        }
+
+        if (transform.position.z < 1.5f)
+        {
+
+            //Set the main Color of the Material to blue
+            rend.material.shader = Shader.Find("_Color");
+            rend.material.SetColor("_Color", Color.blue);
+
+            //Find the Specular shader and change its Color to blue
+            rend.material.shader = Shader.Find("Specular");
+            rend.material.SetColor("_SpecColor", Color.blue);
+        }
+
+        if (transform.position.z < -1.5f)
+        {
+            rend.material.shader = Shader.Find("_Color");
+            rend.material.SetColor("_Color", Color.black);
+            rend.material.shader = Shader.Find("Specular");
+            rend.material.SetColor("_SpecColor", Color.black);
+        }
+
+    }
+     
+    private void CheckKeys(Renderer rend)
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (transform.position.z < 1.5f && transform.position.z > -1.5f)
+            {
+                Debug.Log("Précision : " + transform.position.z);
+                GererCubes.score += Math.Abs(1.5f - transform.position.z);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/scripts/GererCubes.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GererCubes : MonoBehaviour {
+
+    public GameObject MonCubeG;
+    public GameObject MonCubeM;
+    public GameObject MonCubeD;
+    public Text scoreText;
+    public Text finText;
+
+    public static float score;
+
+    private float lastScore;
+    private float nextActionTimeG = 0.0f;
+    private float nextActionTimeM = 0.0f;
+    private float nextActionTimeD = 0.0f;
+    // Use this for initialization
+    void Start () {
+        score = 0;
+        lastScore = -1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (score != lastScore)
+        {
+            SetScoreText();
+            lastScore = score;
+        }
+
+        if (finText.text == "")
+        {
+            if (Time.time > nextActionTimeG)
+            {
+                nextActionTimeG += (3f * UnityEngine.Random.value) + 0.8f;
+                Instantiate(MonCubeG, new Vector3(-3f, 0, 30), Quaternion.identity);
+            }
+
+            if (Time.time > nextActionTimeM)
+            {
+                nextActionTimeM += (3f * UnityEngine.Random.value) + 0.8f;
+                Instantiate(MonCubeM, new Vector3(0, 0, 30), Quaternion.identity);
+            }
+
+            if (Time.time > nextActionTimeD)
+            {
+                nextActionTimeD += (3f * UnityEngine.Random.value) + 0.8f;
+                Instantiate(MonCubeD, new Vector3(3f, 0, 30), Quaternion.identity);
+            }
+        }
+    }
+
+    void SetScoreText()
+    {
+        // Update the text field of our 'countText' variable
+        float truncated = (float)(Math.Truncate((double)score * 100.0) / 100.0);
+        scoreText.text = "Score : " + truncated.ToString();
+
+        // Check if our 'count' is equal to or exceeded 12
+        if (score <= -15)
+        {
+            finText.text = "You Loose :(";
+            Destroy(GameObject.FindWithTag("GenCube"));
+        }
+        if (score >= 15)
+        {
+            finText.text = "You Win!";
+            Destroy(GameObject.FindWithTag("GenCube"));
+        }
+    }
+}
